26f8ec1fbbd712030abe554bbf09a9d062ad3f7a
zhenjie zhu <tech@intellij.io>
2025-12-01 14:36:27 +0800

    Add `EchoHandlerTest` to verify echo functionality in `netty-server-tcp-test` module

---
+++ netty-server-tcp-test/src/test/kotlin/io/intellij/kt/netty/server/test/EchoHandlerTest.kt
@@ -0,0 +1,42 @@
+package io.intellij.kt.netty.server.test
+
+import io.intellij.kt.netty.server.test.handlers.EchoHandler
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
+import io.netty.channel.embedded.EmbeddedChannel
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * EchoHandlerTest
+ *
+ * @author tech@intellij.io
+ */
+class EchoHandlerTest {
+
+    @Throws(Exception::class)
+    @Test
+    fun `test echo handler`() {
+        val msg = "hello world"
+
+        // 1. create EmbeddedChannel
+        val channel = EmbeddedChannel(EchoHandler())
+
+        val input = msg.toByteArray()
+        val inBuf = Unpooled.wrappedBuffer(input)
+
+        // 2. writeInbound and flush
+        channel.writeInbound(inBuf)
+        channel.flush()
+
+        // 3. readOutbound
+        val outBuf = channel.readOutbound<ByteBuf>()
+        val outBytes = ByteArray(outBuf.readableBytes()).also {
+            outBuf.readBytes(it)
+        }
+
+        Assertions.assertEquals(msg, String(outBytes))
+        channel.finish()
+    }
+
+}
