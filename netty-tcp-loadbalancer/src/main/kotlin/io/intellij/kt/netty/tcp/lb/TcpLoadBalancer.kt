c6803167a3f780db697af0f814494c1f4039d6fe
zhenjie zhu <tech@intellij.io>
2025-12-01 22:41:05 +0800

    Refactor `TcpLoadBalancer`: replace `also` with `let`, improve exception handling with `runCatching`, and simplify configuration loading logic.

--- netty-tcp-loadbalancer/src/main/kotlin/io/intellij/kt/netty/tcp/lb/TcpLoadBalancer.kt
+++ netty-tcp-loadbalancer/src/main/kotlin/io/intellij/kt/netty/tcp/lb/TcpLoadBalancer.kt
@@ -4,7 +4,6 @@ import io.intellij.kt.netty.commons.getLogger
 import io.intellij.kt.netty.tcp.lb.config.ConfigParser
 import io.intellij.kt.netty.tcp.lb.handlers.LoadBalancerInitializer
 import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.ChannelFuture
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
@@ -17,28 +16,27 @@ import io.netty.channel.socket.nio.NioServerSocketChannel
 private val log = getLogger("TcpLoadBalancer")
 
 fun main() {
-    ConfigParser.loadConfig("lb-config.json")?.also {
+    ConfigParser.loadConfig("lb-config.json")?.let {
         log.info("lbConfig: {}", it)
+
         val boss = NioEventLoopGroup(1)
         val worker = NioEventLoopGroup()
         val bootstrap = ServerBootstrap()
+            .group(boss, worker)
+            .channel(NioServerSocketChannel::class.java)
+            .childHandler(LoadBalancerInitializer(it.strategy, it.backends))
+            .childOption(ChannelOption.AUTO_READ, false)
 
-        try {
-            bootstrap.group(boss, worker)
-                .channel(NioServerSocketChannel::class.java)
-                .childHandler(LoadBalancerInitializer(it.strategy, it.backends))
-                .childOption(ChannelOption.AUTO_READ, false)
-            val sync: ChannelFuture = bootstrap.bind(it.port).sync()
+        runCatching {
+            val future = bootstrap.bind(it.port).sync()
             log.info("TcpLoadBalancer start on port: {}", it.port)
-            sync.channel().closeFuture().sync()
-        } catch (e: Exception) {
-            log.error("TcpLoadBalancer start error | {}", e.message)
-        } finally {
+            future.channel().closeFuture().sync()
+        }.onFailure { e ->
+            log.error("TcpLoadBalancer start error", e)
+        }.also {
             boss.shutdownGracefully()
             worker.shutdownGracefully()
         }
-    } ?: run {
-        // 加载配置失败时打印
-        log.error("load config failed, exit")
-    }
+    } ?: log.error("load config failed, exit")
+
 }
