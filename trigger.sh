8abe62c697ae3e4eb55f76836469356f7520fcf8
zhenjie zhu <tech@intellij.io>
2026-01-30 12:13:37 +0800

    Refactor: update GitHub Actions workflows to use new commit keywords for conditional concurrency and execution logic.

--- trigger.sh
+++ trigger.sh
@@ -8,10 +8,10 @@ branch="main"
 
 # 可用关键字列表
 key_word_list=(
-  "build_tcp_server_test"
-  "build_tcp_proxy"
-  "build_socks_server"
-  "delete_runs"
+  "@ci:img:build:tcp_server_test"
+  "@ci:img:build:tcp_proxy"
+  "@ci:img:build:socks_server"
+  "@ci:ops:cleanup:runs"
 )
 
 trigger_git_commit "key_word_list" "$branch" "$@"
